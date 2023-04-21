4a42499c4ff31270430d858534f74d8598014320
poorants <donghun.kim.w@gmail.com>
2023-04-21 10:51:51 +0900

    first commit

---
+++ bin/code_organizer.dart
@@ -0,0 +1,5 @@
+import 'package:code_organizer/code_organizer.dart' as code_organizer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${code_organizer.calculate()}!');
+}
---
+++ test/code_organizer_test.dart
@@ -0,0 +1,8 @@
+import 'package:code_organizer/code_organizer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
